9e7722f3903fbdb659c0f65d134627ebb78ad2e0
Martin Moene <martin.moene@gmail.com>
2014-08-31 22:01:21 +0200

    Update copyright year

--- tests/test_lest_decompose.cpp
+++ tests/test_lest_decompose.cpp
@@ -1,4 +1,4 @@
-// Copyright 2013 by Martin Moene
+// Copyright 2013, 2014 by Martin Moene
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
