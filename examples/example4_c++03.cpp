47673057cac63d1544b5e36671f7035b20e38319
Martin Moene <martin.moene@gmail.com>
2014-09-29 18:14:39 +0200

    Don't 'use' namespace lest

--- examples/example4_c++03.cpp
+++ examples/example4_c++03.cpp
@@ -2,8 +2,6 @@
 
 #include "lest_cpp03.hpp"
 
-using namespace lest;
-
 char a() { return 'a'; }
 char b() { return 'b'; }
 
@@ -28,7 +26,7 @@ char const* world = "world";
 
 #define CASE( name ) lest_CASE( specification, name )
 
-test_specification specification;
+lest::test_specification specification;
 
 CASE( "character" )
 {
@@ -118,15 +116,15 @@ int main( int argc, char * argv[] )
 // g++ -Wall -Wextra -std=c++11 -I.. -o example4_c++03.exe example4_c++03.cpp && example4_c++03
 // g++ -Wall -Wextra -std=c++03 -I.. -o example4_c++03.exe example4_c++03.cpp && example4_c++03
 
-// example4_c++03.cpp:36: failed: character: b() < a() for 'b' < 'a'
-// example4_c++03.cpp:42: failed: integral: x() == y() for 3 == 7
-// example4_c++03.cpp:48: failed: real: pi() != pi() for 3.14 != 3.14
-// example4_c++03.cpp:54: failed: mixed real, integral: pi() == x() for 3.14 == 3
-// example4_c++03.cpp:60: failed: mixed integral, real: x() == pi() for 3 == 3.14
-// example4_c++03.cpp:66: failed: boolean: t() == f() for true == false
-// example4_c++03.cpp:76: failed: pointer: ptr() != (void*)NULL for 0 != 0
-// example4_c++03.cpp:84: failed: class object pointer: &a == &b for 0x28fc27 == 0x28fc26
-// example4_c++03.cpp:90: failed: std::string: std_world < std_hello for "world" < "hello"
-// example4_c++03.cpp:96: failed: C-string: world < std_hello for "world" < "hello"
-// example4_c++03.cpp:108: failed: duplicate evaluation: 2 == ++n for 2 == 1
+// example4_c++03.cpp(34): failed: character: b() < a() for 'b' < 'a'
+// example4_c++03.cpp(40): failed: integral: x() == y() for 3 == 7
+// example4_c++03.cpp(46): failed: real: pi() != pi() for 3.14 != 3.14
+// example4_c++03.cpp(52): failed: mixed real, integral: pi() == x() for 3.14 == 3
+// example4_c++03.cpp(58): failed: mixed integral, real: x() == pi() for 3 == 3.14
+// example4_c++03.cpp(64): failed: boolean: t() == f() for true == false
+// example4_c++03.cpp(74): failed: pointer: ptr() != (void*)NULL for 00000000 != 00000000
+// example4_c++03.cpp(82): failed: class object pointer: &a == &b for 0018FCB8 == 0018FCB4
+// example4_c++03.cpp(88): failed: std::string: std_world < std_hello for "world" < "hello"
+// example4_c++03.cpp(94): failed: C-string: world < std_hello for "world" < "hello"
+// example4_c++03.cpp(106): failed: duplicate evaluation: 2 == ++n for 2 == 1
 // 11 out of 12 selected tests failed.
