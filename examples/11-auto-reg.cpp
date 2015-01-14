4a57fbdd8b49bb82ca96ff628be3e923514cb40d
Martin Moene <martin.moene@gmail.com>
2015-01-14 21:19:56 +0100

    Add single-file auto registration example

---
+++ examples/11-auto-reg.cpp
@@ -0,0 +1,25 @@
+// C++11 - auto-register test cases.
+
+#include "lest.hpp"
+
+#define CASE( name ) lest_CASE( specification, name )
+
+static lest::tests specification; 
+
+CASE( "A passing test" "[pass]" ) 
+{
+    EXPECT( 42 == 42 );
+}
+
+CASE( "A failing test" "[fail]" ) 
+{
+    EXPECT( 42 == 7 );
+}
+
+int main( int argc, char * argv[] )
+{
+    return lest::run( specification, argc, argv /*, std::cout */ );
+}
+
+// cl -nologo -W3 -EHsc -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -Fe11-auto-reg.exe 11-auto-reg.cpp && 11-auto-reg
+// g++ -Wall -Wextra -std=c++11 -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -o 11-auto-reg.exe 11-auto-reg.cpp && 11-auto-reg
