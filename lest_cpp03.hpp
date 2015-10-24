b271fbcd700023fbe86b1fae6a4b38e6470e707d
Martin Moene <martin.moene@gmail.com>
2015-10-24 15:43:04 +0200

    Release 1.24.2

--- lest_cpp03.hpp
+++ lest_cpp03.hpp
@@ -36,7 +36,7 @@
 # pragma GCC   diagnostic ignored "-Wunused-value"
 #endif
 
-#define  lest_VERSION "1.24.1"
+#define  lest_VERSION "1.24.2"
 
 #ifndef  lest_FEATURE_COLOURISE
 # define lest_FEATURE_COLOURISE 0
