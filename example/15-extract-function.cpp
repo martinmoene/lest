8e1e6ee873c15fb6c72e547bba2eb41df9803400
Martin Moene <martin.moene@gmail.com>
2015-11-24 00:12:26 +0100

    Add examples with extracted function/lambda
    See issues #23, #24 (Thanks to @MariusDe)

---
+++ example/15-extract-function.cpp
@@ -0,0 +1,37 @@
+// C++11 - call a function that uses EXPECT().
+
+#include "lest.hpp"
+
+#define CASE( name ) lest_CASE( specification, name )
+
+static lest::tests specification; 
+
+void fun( lest::env & $, int a, int b )
+{
+    EXPECT( a == b );
+}
+
+CASE( "Call a function that uses EXPECT" )
+{
+    fun( $, 1, 1 );
+    fun( $, 1, 2 );
+}
+
+// or, via a macro if you like:
+
+#define mfun( a, b ) \
+    fun( $, a, b )
+
+CASE( "Call a macro that calls a function that uses EXPECT" )
+{
+    mfun( 1, 1 );
+    mfun( 1, 2 );
+}
+
+int main( int argc, char * argv[] )
+{
+    return lest::run( specification, argc, argv /*, std::cout */ );
+}
+
+// cl -nologo -W3 -EHsc -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -Fe15-extract-function.exe 15-extract-function.cpp && 15-extract-function
+// g++ -Wall -Wextra -std=c++11 -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -o 15-extract-function.exe 15-extract-function.cpp && 15-extract-function
---
+++ example/15-extract-lambda.cpp
@@ -0,0 +1,23 @@
+// C++11 - call a lambda that uses EXPECT().
+
+#include "lest.hpp"
+
+#define CASE( name ) lest_CASE( specification, name )
+
+static lest::tests specification; 
+
+CASE( "Call a lambda that uses EXPECT" )
+{
+    auto fun = [ &$ ]( int a, int b ){ EXPECT( a == b ); };
+
+    fun( 1, 1 );
+    fun( 1, 2 );
+}
+
+int main( int argc, char * argv[] )
+{
+    return lest::run( specification, argc, argv /*, std::cout */ );
+}
+
+// cl -nologo -W3 -EHsc -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -Fe15-extract-lambda.exe 15-extract-lambda.cpp && 15-extract-lambda
+// g++ -Wall -Wextra -std=c++11 -Dlest_FEATURE_AUTO_REGISTER=1 -I.. -o 15-extract-lambda.exe 15-extract-lambda.cpp && 15-extract-lambda
