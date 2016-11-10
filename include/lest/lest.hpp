d3668cfb35ebc9fecbb020eab1e6dada5c486fbf
Martin Moene <martin.moene@gmail.com>
2016-11-10 22:17:32 +0100

    Release 1.27.1

--- include/lest/lest.hpp
+++ include/lest/lest.hpp
@@ -40,7 +40,7 @@
 # pragma GCC   diagnostic ignored "-Wunused-value"
 #endif
 
-#define  lest_VERSION "1.27.x"
+#define  lest_VERSION "1.27.1"
 
 #ifndef  lest_FEATURE_AUTO_REGISTER
 # define lest_FEATURE_AUTO_REGISTER  0
--- include/lest/lest_cpp03.hpp
+++ include/lest/lest_cpp03.hpp
@@ -35,7 +35,7 @@
 # pragma GCC   diagnostic ignored "-Wunused-value"
 #endif
 
-#define  lest_VERSION "1.27.x"
+#define  lest_VERSION "1.27.1"
 
 #ifndef  lest_FEATURE_COLOURISE
 # define lest_FEATURE_COLOURISE 0
