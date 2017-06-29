5e58296e731e88115b3398c8b7fc6b2714900664
Martin Moene <martin.moene@gmail.com>
2017-06-29 20:35:43 +0200

    Release 1.30.1

--- include/lest/lest.hpp
+++ include/lest/lest.hpp
@@ -40,7 +40,7 @@
 # pragma GCC   diagnostic ignored "-Wunused-value"
 #endif
 
-#define  lest_VERSION "1.30.0"
+#define  lest_VERSION "1.30.1"
 
 #ifndef  lest_FEATURE_AUTO_REGISTER
 # define lest_FEATURE_AUTO_REGISTER  0
