3a3652534cca93bad39485145cea9eb68ce5f904
Martin Moene <martin.moene@gmail.com>
2017-07-21 15:17:27 +0200

    Update for renamed license file

--- include/lest/lest_cpp03.hpp
+++ include/lest/lest_cpp03.hpp
@@ -4,7 +4,7 @@
 // http://skillsmatter.com/podcast/agile-testing/kevlin-henney-rethinking-unit-testing-in-c-plus-plus
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef LEST_LEST_HPP_INCLUDED
 #define LEST_LEST_HPP_INCLUDED
--- test/test_lest_cpp03.cpp
+++ test/test_lest_cpp03.cpp
@@ -1,7 +1,7 @@
 // Copyright 2013, 2014, 2015 by Martin Moene
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include "lest_cpp03.hpp"
 #include <string>
