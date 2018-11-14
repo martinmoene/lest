4928146f33dba8e1b233e79c9c1fb456b821093f
Martin Moene <martin.moene@gmail.com>
2018-11-14 13:48:53 +0100

    Release 1.33.4

--- include/lest/lest.hpp
+++ include/lest/lest.hpp
@@ -31,7 +31,7 @@
 #include <cmath>
 #include <cstddef>
 
-#define  lest_VERSION "1.33.3"
+#define  lest_VERSION "1.33.4"
 
 #ifndef  lest_FEATURE_AUTO_REGISTER
 # define lest_FEATURE_AUTO_REGISTER  0
